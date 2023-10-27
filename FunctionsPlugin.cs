9e573af2cd08a3ee08e571aafd488ed57c71ef81
RTMecha <mechaprower2022@gmail.com>
2023-10-28 04:30:10 +1100

    Version: 1.6.1

--- FunctionsPlugin.cs
+++ FunctionsPlugin.cs
@@ -31,7 +31,7 @@ using Ease = RTFunctions.Functions.Animation.Ease;
 
 namespace RTFunctions
 {
-	[BepInPlugin("com.mecha.rtfunctions", "RT Functions", " 1.6.0")]
+	[BepInPlugin("com.mecha.rtfunctions", "RT Functions", " 1.6.1")]
 	[BepInProcess("Project Arrhythmia.exe")]
 	public class FunctionsPlugin : BaseUnityPlugin
 	{
