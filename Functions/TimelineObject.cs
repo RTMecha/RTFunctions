b88bdfad42adbcdd8113e74279b8d8509b1bfe11
RTMecha <mechaprower2022@gmail.com>
2023-12-09 09:31:40 +1100

    Changed TimelineObject

--- Functions/TimelineObject.cs
+++ Functions/TimelineObject.cs
@@ -17,21 +17,21 @@ using BasePrefabObject = DataManager.GameData.PrefabObject;
 
 namespace RTFunctions.Functions
 {
-    public class TimelineObject<T> : Exists
+    public class TimelineObject : Exists
     {
-        public TimelineObject(T data)
+        public TimelineObject(object data)
         {
             Data = data;
         }
 
-        public TimelineObject(T data, GameObject gameObject, Image image)
+        public TimelineObject(object data, GameObject gameObject, Image image)
         {
             Data = data;
             GameObject = gameObject;
             Image = image;
         }
         
-        public TimelineObject(T data, int type, GameObject gameObject, Image image)
+        public TimelineObject(object data, int type, GameObject gameObject, Image image)
         {
             Data = data;
             Type = type;
@@ -39,7 +39,9 @@ namespace RTFunctions.Functions
             Image = image;
         }
 
-        public T Data { get; set; }
+        public T GetData<T>() => (T)Data;
+
+        public object Data { get; set; }
         public GameObject GameObject { get; set; }
         public Image Image { get; set; }
 
@@ -51,6 +53,8 @@ namespace RTFunctions.Functions
                     return (Data as BaseBeatmapObject).id;
                 if (IsPrefabObject)
                     return (Data as BasePrefabObject).ID;
+                if (IsEventKeyframe)
+                    return (Data as EventKeyframe).id;
                 return "";
             }
         }
@@ -84,6 +88,38 @@ namespace RTFunctions.Functions
                     return (Data as EventKeyframe).eventTime;
                 return -1;
             }
+            set
+            {
+                if (IsBeatmapObject)
+                    (Data as BeatmapObject).StartTime = value;
+                if (IsPrefabObject)
+                    (Data as PrefabObject).StartTime = value;
+                if (IsEventKeyframe)
+                    (Data as EventKeyframe).eventTime = value;
+            }
+        }
+
+        public int Bin
+        {
+            get
+            {
+                if (IsBeatmapObject)
+                    return (Data as BeatmapObject).editorData.Bin;
+                if (IsPrefabObject)
+                    return (Data as PrefabObject).editorData.Bin;
+                if (IsEventKeyframe)
+                    return Type;
+                return -1;
+            }
+            set
+            {
+                if (IsBeatmapObject)
+                    (Data as BeatmapObject).editorData.Bin = value;
+                if (IsPrefabObject)
+                    (Data as PrefabObject).editorData.Bin = value;
+                if (IsEventKeyframe)
+                    Type = value;
+            }
         }
 
         public int Layer
@@ -99,13 +135,60 @@ namespace RTFunctions.Functions
                 return -1;
             }
         }
+
+        public bool Locked
+        {
+            get
+            {
+                if (IsBeatmapObject)
+                    return (Data as BeatmapObject).editorData.locked;
+                if (IsPrefabObject)
+                    return (Data as PrefabObject).editorData.locked;
+                return false;
+            }
+            set
+            {
+                if (IsBeatmapObject)
+                    (Data as BeatmapObject).editorData.locked = value;
+                if (IsPrefabObject)
+                    (Data as PrefabObject).editorData.locked = value;
+            }
+        }
+
+        public bool Collapse
+        {
+            get
+            {
+                if (IsBeatmapObject)
+                    return (Data as BeatmapObject).editorData.collapse;
+                if (IsPrefabObject)
+                    return (Data as PrefabObject).editorData.collapse;
+                return false;
+            }
+            set
+            {
+                if (IsBeatmapObject)
+                    (Data as BeatmapObject).editorData.collapse = value;
+                if (IsPrefabObject)
+                    (Data as PrefabObject).editorData.collapse = value;
+            }
+        }
+
         public float timeOffset;
         public int binOffset;
 
         public bool selected;
 
+        float zoom = 0.05f;
+        public float Zoom { get => zoom; set => zoom = value; }
+
+        float timelinePosition;
+        public float TimelinePosition { get => timelinePosition; set => timelinePosition = value; }
+
         public bool IsBeatmapObject => Data != null && Data is BeatmapObject;
         public bool IsPrefabObject => Data != null && Data is PrefabObject;
         public bool IsEventKeyframe => Data != null && Data is EventKeyframe;
+
+        public List<TimelineObject> InternalSelections { get; set; } = new List<TimelineObject>();
     }
 }
