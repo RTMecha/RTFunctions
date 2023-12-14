c5293c6ba5e28fac7e626401651be3fe83e73b6f
RTMecha <mechaprower2022@gmail.com>
2023-12-14 21:29:22 +1100

    TryParse with a return value

--- Functions/Managers/Parser.cs
+++ Functions/Managers/Parser.cs
@@ -32,6 +32,27 @@ namespace RTFunctions.Functions.Managers
 {
     public static class Parser
 	{
+		public static int TryParse(string input, int defaultValue)
+        {
+			if (int.TryParse(input, out int num))
+				return num;
+			return defaultValue;
+        }
+		
+		public static float TryParse(string input, float defaultValue)
+        {
+			if (float.TryParse(input, out float num))
+				return num;
+			return defaultValue;
+        }
+
+		public static bool TryParse(string input, bool defaultValue)
+        {
+			if (bool.TryParse(input, out bool result))
+				return result;
+			return defaultValue;
+        }
+
 		public static IEnumerator ParseRTFunctionsData(JSONNode _rt)
         {
 			if (!string.IsNullOrEmpty(_rt["v"]))
