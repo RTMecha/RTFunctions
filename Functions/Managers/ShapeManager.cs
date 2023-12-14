feb97fa87a66271c26090086f0c9c1bea3fcf5b0
RTMecha <mechaprower2022@gmail.com>
2023-12-14 21:29:55 +1100

    Setup 3D shapes

--- Functions/Managers/ShapeManager.cs
+++ Functions/Managers/ShapeManager.cs
@@ -37,11 +37,14 @@ namespace RTFunctions.Functions.Managers
         bool gameHasLoaded;
         bool loadedShapes;
 
-        void Awake() => inst = this;
+        void Awake()
+        {
+            inst = this;
+        }
 
         void Start()
         {
-            SetupMeshes();
+
         }
 
         void Update()
@@ -54,25 +57,6 @@ namespace RTFunctions.Functions.Managers
             else if (!ObjectManager.inst)
             {
                 gameHasLoaded = false;
-            }
-        }
-
-        void SetupMeshes()
-        {
-            shapes3D = new List<Shape>();
-            shapes = new List<Shape>();
-
-            var files = System.IO.Directory.GetFiles(RTFile.ApplicationDirectory + "BepInEx/plugins/Assets/Shape Assets", "*.lssh");
-
-            foreach (var file in files)
-            {
-                var jn = SimpleJSON.JSON.Parse(RTFile.ReadFromFile(file));
-                var shape = Shape.Parse(jn);
-
-                if (file.Replace("\\", "/").Contains("Shape Assets/bg_"))
-                    shapes3D.Add(shape);
-                else
-                    shapes.Add(shape);
             }
         }
 
@@ -130,80 +114,169 @@ namespace RTFunctions.Functions.Managers
                 System.Windows.Forms.MessageBox.Show("Shapes Setup file does not exist.\nYou may run into issues with playing the game from here on, so it is recommended to\ndownload the proper assets from Github and place them into the appropriate folders.", "Error!");
 
             Shapes2D = new List<List<Shape>>();
+            Shapes3D = new List<List<Shape>>();
 
             var jn = JSON.Parse(RTFile.ReadFromFile(RTFile.ApplicationDirectory + ShapesSetup));
             for (int i = 0; i < jn["type"].Count; i++)
             {
                 Shapes2D.Add(new List<Shape>());
+                Shapes3D.Add(new List<Shape>());
                 for (int j = 0; j < jn["type"][i]["option"].Count; j++)
                 {
                     var fullPath = RTFile.ApplicationDirectory + jn["type"][i]["option"][j]["path"];
 
-                    Debug.Log($"{FunctionsPlugin.className}Loading shape from: {jn["type"][i]["option"][j]["path"]}");
-                    var sjn = JSON.Parse(RTFile.ReadFromFile(fullPath + "/data.lssh"));
-
-                    Mesh mesh = null;
-                    if (i != 4 && i != 6 && sjn["verts"] != null && sjn["tris"] != null)
+                    //Debug.Log($"{FunctionsPlugin.className}Loading shape from: {jn["type"][i]["option"][j]["path"]}");
+                    // 2D
                     {
-                        Debug.Log($"{FunctionsPlugin.className}Setting mesh data for {sjn["name"]}");
-                        mesh = new Mesh();
-                        mesh.name = sjn["name"];
-                        Vector3[] vertices = new Vector3[sjn["verts"].Count];
-                        for (int k = 0; k < sjn["verts"].Count; k++)
-                            vertices[k] = new Vector3(sjn["verts"][k]["x"].AsFloat, sjn["verts"][k]["y"].AsFloat, sjn["verts"][k]["z"].AsFloat);
+                        var sjn = JSON.Parse(RTFile.ReadFromFile(fullPath + "/data.lssh"));
 
-                        int[] triangles = new int[sjn["tris"].Count];
-                        for (int k = 0; k < sjn["tris"].Count; k++)
-                            triangles[k] = sjn["tris"][k].AsInt;
+                        Mesh mesh = null;
+                        if (i != 4 && i != 6 && sjn["verts"] != null && sjn["tris"] != null)
+                        {
+                            //Debug.Log($"{FunctionsPlugin.className}Setting mesh data for {sjn["name"]}");
+                            mesh = new Mesh();
+                            mesh.name = sjn["name"];
+                            Vector3[] vertices = new Vector3[sjn["verts"].Count];
+                            for (int k = 0; k < sjn["verts"].Count; k++)
+                                vertices[k] = new Vector3(sjn["verts"][k]["x"].AsFloat, sjn["verts"][k]["y"].AsFloat, sjn["verts"][k]["z"].AsFloat);
 
-                        mesh.vertices = vertices;
-                        mesh.triangles = triangles;
+                            int[] triangles = new int[sjn["tris"].Count];
+                            for (int k = 0; k < sjn["tris"].Count; k++)
+                                triangles[k] = sjn["tris"][k].AsInt;
+
+                            mesh.vertices = vertices;
+                            mesh.triangles = triangles;
+                        }
+
+                        if (ObjectManager.inst.objectPrefabs.Count < i + 1)
+                        {
+                            //Debug.Log($"{FunctionsPlugin.className}Adding new ObjectPrefabHolder [{i}]");
+                            var p = new ObjectManager.ObjectPrefabHolder();
+                            p.options = new List<GameObject>();
+                            ObjectManager.inst.objectPrefabs.Add(p);
+                        }
+
+                        if (ObjectManager.inst.objectPrefabs[i].options.Count < j + 1 && mesh != null)
+                        {
+                            //Debug.Log($"{FunctionsPlugin.className}Adding new ObjectPrefab [{i}, {j}]");
+                            var gameObject = ObjectManager.inst.objectPrefabs[1].options[0].Duplicate(null, sjn["name"]);
+
+                            gameObject.transform.GetChild(0).GetComponent<MeshFilter>().mesh = mesh;
+                            gameObject.transform.GetChild(0).GetComponent<PolygonCollider2D>().points = mesh.vertices.Select(x => new Vector2(x.x, x.y)).ToArray();
+
+                            //gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+                            ObjectManager.inst.objectPrefabs[i].options.Add(gameObject);
+                        }
+
+                        if (i == 6 && EditorManager.inst == null)
+                        {
+                            var imageMesh = new GameObject("mesh");
+                            imageMesh.layer = 8;
+                            var imageObject = new GameObject("object");
+                            imageObject.layer = 8;
+                            imageObject.transform.SetParent(imageMesh.transform);
+
+                            imageObject.AddComponent<SpriteRenderer>();
+                            var imageRB = imageObject.AddComponent<Rigidbody2D>();
+
+                            imageRB.angularDrag = 0f;
+                            imageRB.bodyType = RigidbodyType2D.Kinematic;
+                            imageRB.gravityScale = 0f;
+                            imageRB.inertia = 0f;
+
+                            ObjectManager.inst.objectPrefabs[6].options.Add(imageMesh);
+                        }
+
+                        if (!loadedShapes)
+                        {
+                            var shape = new Shape(sjn["name"], i, j, mesh, null, string.IsNullOrEmpty(sjn["p"]) ? Shape.Property.RegularObject : (Shape.Property)sjn["p"].AsInt);
+
+                            if (RTFile.FileExists(fullPath + "/icon.png"))
+                            {
+                                shape.Icon = SpriteManager.LoadSprite(fullPath + "/icon.png");
+                            //    Debug.Log($"{FunctionsPlugin.className}Setting Icon for {sjn["name"]}");
+                            //    Networking.AlephNetworkManager.inst.StartCoroutine(Networking.AlephNetworkManager.DownloadImageTexture("file://" + fullPath + "/icon.png", delegate (Texture2D texture2D)
+                            //    {
+                            //        var s = Shapes2D[i][j];
+                            //        s.Icon = RTSpriteManager.CreateSprite(texture2D);
+                            //        Shapes2D[i][j] = s;
+                            //    }));
+                            }
+
+                            shape.GameObject = ObjectManager.inst.objectPrefabs[i].options[j];
+
+                            Shapes2D[i].Add(shape);
+                        }
                     }
 
-                    if (ObjectManager.inst.objectPrefabs.Count < i + 1)
+                    // 3D
+                    if (RTFile.FileExists(fullPath + "/bg_data.lssh"))
                     {
-                        Debug.Log($"{FunctionsPlugin.className}Adding new ObjectPrefabHolder [{i}]");
-                        var p = new ObjectManager.ObjectPrefabHolder();
-                        p.options = new List<GameObject>();
-                        ObjectManager.inst.objectPrefabs.Add(p);
+                        var sjn = JSON.Parse(RTFile.ReadFromFile(fullPath + "/bg_data.lssh"));
+
+                        Mesh mesh = null;
+                        if (i != 4 && i != 6 && sjn["verts"] != null && sjn["tris"] != null)
+                        {
+                            //Debug.Log($"{FunctionsPlugin.className}Setting mesh data for {sjn["name"]}");
+                            mesh = new Mesh();
+                            mesh.name = sjn["name"];
+                            Vector3[] vertices = new Vector3[sjn["verts"].Count];
+                            for (int k = 0; k < sjn["verts"].Count; k++)
+                                vertices[k] = new Vector3(sjn["verts"][k]["x"].AsFloat, sjn["verts"][k]["y"].AsFloat, sjn["verts"][k]["z"].AsFloat);
+
+                            int[] triangles = new int[sjn["tris"].Count];
+                            for (int k = 0; k < sjn["tris"].Count; k++)
+                                triangles[k] = sjn["tris"][k].AsInt;
+
+                            mesh.vertices = vertices;
+                            mesh.triangles = triangles;
+                        }
+
+                        if (!loadedShapes)
+                        {
+                            var shape = new Shape(sjn["name"], i, j, mesh, null, string.IsNullOrEmpty(sjn["p"]) ? Shape.Property.RegularObject : (Shape.Property)sjn["p"].AsInt);
+
+                            if (RTFile.FileExists(fullPath + "/icon.png"))
+                            {
+                                shape.Icon = SpriteManager.LoadSprite(fullPath + "/icon.png");
+                                //    Debug.Log($"{FunctionsPlugin.className}Setting Icon for {sjn["name"]}");
+                                //    Networking.AlephNetworkManager.inst.StartCoroutine(Networking.AlephNetworkManager.DownloadImageTexture("file://" + fullPath + "/icon.png", delegate (Texture2D texture2D)
+                                //    {
+                                //        var s = Shapes2D[i][j];
+                                //        s.Icon = RTSpriteManager.CreateSprite(texture2D);
+                                //        Shapes2D[i][j] = s;
+                                //    }));
+                            }
+
+                            Shapes3D[i].Add(shape);
+                        }
                     }
-
-                    if (ObjectManager.inst.objectPrefabs[i].options.Count < j + 1 && mesh != null)
+                    else if (!loadedShapes)
                     {
-                        Debug.Log($"{FunctionsPlugin.className}Adding new ObjectPrefab [{i}, {j}]");
-                        var gameObject = ObjectManager.inst.objectPrefabs[1].options[0].Duplicate(null, sjn["name"]);
+                        if (!loadedShapes)
+                        {
+                            var shape = new Shape("null", i, j, null, null, i == 4 ? Shape.Property.TextObject : i == 6 ? Shape.Property.ImageObject : Shape.Property.RegularObject);
 
-                        gameObject.transform.GetChild(0).GetComponent<MeshFilter>().mesh = mesh;
-                        gameObject.transform.GetChild(0).GetComponent<PolygonCollider2D>().points = mesh.vertices.Select(x => new Vector2(x.x, x.y)).ToArray();
+                            if (RTFile.FileExists(fullPath + "/icon.png"))
+                            {
+                                shape.Icon = SpriteManager.LoadSprite(fullPath + "/icon.png");
+                                //    Debug.Log($"{FunctionsPlugin.className}Setting Icon for {sjn["name"]}");
+                                //    Networking.AlephNetworkManager.inst.StartCoroutine(Networking.AlephNetworkManager.DownloadImageTexture("file://" + fullPath + "/icon.png", delegate (Texture2D texture2D)
+                                //    {
+                                //        var s = Shapes2D[i][j];
+                                //        s.Icon = RTSpriteManager.CreateSprite(texture2D);
+                                //        Shapes2D[i][j] = s;
+                                //    }));
+                            }
 
-                        gameObject.hideFlags = HideFlags.HideAndDontSave;
-
-                        ObjectManager.inst.objectPrefabs[i].options.Add(gameObject);
-                    }
-
-                    if (!loadedShapes)
-                    {
-                        var shape = new Shape(sjn["name"], sjn["s"].AsInt, sjn["so"].AsInt, mesh, null, string.IsNullOrEmpty(sjn["p"]) ? Shape.Property.RegularObject : (Shape.Property)sjn["p"].AsInt);
-
-                        //if (RTFile.FileExists(fullPath + "/icon.png"))
-                        //{
-                        //    Debug.Log($"{FunctionsPlugin.className}Setting Icon for {sjn["name"]}");
-                        //    Networking.AlephNetworkManager.inst.StartCoroutine(Networking.AlephNetworkManager.DownloadImageTexture("file://" + fullPath + "/icon.png", delegate (Texture2D texture2D)
-                        //    {
-                        //        var s = Shapes2D[i][j];
-                        //        s.Icon = RTSpriteManager.CreateSprite(texture2D);
-                        //        Shapes2D[i][j] = s;
-                        //    }));
-                        //}
-
-                        shape.GameObject = ObjectManager.inst.objectPrefabs[i].options[j];
-
-                        Shapes2D[i].Add(shape);
+                            Shapes3D[i].Add(shape);
+                        }
                     }
                 }
             }
 
-            StartCoroutine(LoadIcons());
+            //StartCoroutine(LoadIcons());
 
             loadedShapes = true;
         }
@@ -217,12 +290,20 @@ namespace RTFunctions.Functions.Managers
                     string fullPath = RTFile.ApplicationDirectory + ShapesPath + Shapes2D[i][j].name + "/icon.png";
                     if (RTFile.FileExists(fullPath))
                     {
-                        Debug.Log($"{FunctionsPlugin.className}Setting Icon for {Shapes2D[i][j].name}");
+                        //Debug.Log($"{FunctionsPlugin.className}Setting Icon for {Shapes2D[i][j].name}");
                         yield return Networking.AlephNetworkManager.inst.StartCoroutine(Networking.AlephNetworkManager.DownloadImageTexture("file://" + fullPath, delegate (Texture2D texture2D)
                         {
+                            var sprite = SpriteManager.CreateSprite(texture2D);
                             var s = Shapes2D[i][j];
-                            s.Icon = RTSpriteManager.CreateSprite(texture2D);
+                            s.Icon = sprite;
                             Shapes2D[i][j] = s;
+
+                            if (Shapes3D.Count > i && Shapes3D[i].Count > j)
+                            {
+                                var s3 = Shapes3D[i][j];
+                                s3.Icon = sprite;
+                                Shapes3D[i][j] = s3;
+                            }
                         }));
                     }
                 }
@@ -240,14 +321,9 @@ namespace RTFunctions.Functions.Managers
             set => shapes = value;
         }
         
-        static List<Shape> shapes3D;
-        public static List<Shape> Shapes3D
-        {
-            get => shapes3D;
-            set => shapes3D = value;
-        }
+        public List<List<Shape>> Shapes3D { get; set; }
 
-        public static Shape GetShape(int shape, int shapeOption)
+        public Shape GetShape(int shape, int shapeOption)
         {
             if (!Shapes.Has(x => x.Type == shape && x.Option == shapeOption))
                 return Shapes[0];
@@ -255,6 +331,12 @@ namespace RTFunctions.Functions.Managers
             return Shapes.Find(x => x.Type == shape && x.Option == shapeOption);
         }
 
-        public static Shape GetShape3D(int shape, int shapeOption) => Shapes3D.Find(x => x.Type == shape && x.Option == shapeOption);
+        public Shape GetShape3D(int shape, int shapeOption)
+        {
+            shape = Mathf.Clamp(shape, 0, Shapes3D.Count - 1);
+            shapeOption = Mathf.Clamp(shapeOption, 0, Shapes3D[shape].Count - 1);
+
+            return Shapes3D[shape][shapeOption];
+        }
     }
 }
