906bd6ac0d59c6e1ab8cfed38314c08707e1a2c7
RTMecha <mechaprower2022@gmail.com>
2023-12-23 06:59:26 +1100

    This is so we don't have to spam GetComponent

---
+++ Functions/Managers/GameStorageManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RTFunctions.Functions.Managers
+{
+    public class GameStorageManager : MonoBehaviour
+    {
+        public static GameStorageManager inst;
+
+        void Awake()
+        {
+            inst = this;
+            perspectiveCam = GameManager.inst.CameraPerspective.GetComponent<Camera>();
+        }
+
+        public Camera perspectiveCam;
+
+    }
+}
