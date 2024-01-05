0bfa89a190b775397752e240fb3640af5ef687c9
RTMecha <mechaprower2022@gmail.com>
2024-01-05 19:33:17 +1100

    Added Debugger for testing purposes

---
+++ Functions/Managers/RTDebugger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTFunctions.Functions.Managers
+{
+    public static class RTDebugger
+    {
+        public static void TogglePlay() => (AudioManager.inst.CurrentAudioSource.isPlaying ? (Action)AudioManager.inst.CurrentAudioSource.Pause : AudioManager.inst.CurrentAudioSource.Play).Invoke();
+    }
+}
