c0d389a1d0aaadcdabad722ce2cb42489d4dd12b
RTMecha <mechaprower2022@gmail.com>
2024-01-05 13:50:06 +1100

    Changed sequence to account for homing keyframes

--- Functions/Animation/Sequence.cs
+++ Functions/Animation/Sequence.cs
@@ -14,6 +14,9 @@ namespace RTFunctions.Functions.Animation
     {
         public readonly IKeyframe<T>[] keyframes;
 
+        public T Value { get; set; }
+        public float Time { get; set; }
+
         public Sequence(IEnumerable<IKeyframe<T>> keyframes)
         {
             this.keyframes = keyframes.ToArray();
@@ -28,27 +31,67 @@ namespace RTFunctions.Functions.Animation
                 throw new NoKeyframeException("Cannot interpolate in an empty sequence!");
             }
 
-            if (keyframes.Length == 1)
-            {
-                return ResultFromSingleKeyframe(keyframes[0]);
-            }
+            Time = time;
+            for (int i = 0; i < keyframes.Length; i++)
+                if (time < keyframes[i].Time)
+                    keyframes[i].Active = false;
 
-            if (time < keyframes[0].Time)
+            //if (time < keyframes[0].Time)
+            //    for (int i = 0; i < keyframes.Length; i++)
+            //        keyframes[i].Active = false;
+
+            if (keyframes.Length == 1 || time < keyframes[0].Time)
             {
-                return ResultFromSingleKeyframe(keyframes[0]);
+                //if (keyframes.Length > 1)
+                //    for (int i = 1; i < keyframes.Length; i++)
+                //        keyframes[i].Active = false;
+
+                if (!keyframes[0].Active)
+                {
+                    keyframes[0].Active = true;
+                    keyframes[0].Start();
+                }
+                Value = ResultFromSingleKeyframe(keyframes[0], 1f);
+                return Value;
             }
 
             if (time >= keyframes[keyframes.Length - 1].Time)
             {
-                return ResultFromSingleKeyframe(keyframes[keyframes.Length - 1]);
+                //if (keyframes.Length - 1 > 0)
+                //    for (int i = 1; i < keyframes.Length - 1; i++)
+                //        keyframes[i].Active = false;
+
+                if (!keyframes[keyframes.Length - 1].Active)
+                {
+                    keyframes[keyframes.Length - 1].Active = true;
+                    keyframes[keyframes.Length - 1].Start();
+                }
+                Value = ResultFromSingleKeyframe(keyframes[keyframes.Length - 1], 0.0f);
+                return Value;
             }
+
+            //keyframes.Where(x => x.Time < time).ToList().ForEach(x =>
+            //{
+            //    x.Active = false;
+            //});
 
             int index = Search(time);
             IKeyframe<T> first = keyframes[index];
             IKeyframe<T> second = keyframes[index + 1];
 
+            //if (index - 1 > 0)
+            //    for (int i = 0; i < index; i++)
+            //        keyframes[i].Active = false;
+
+            if (!first.Active)
+            {
+                first.Active = true;
+                first.Start();
+            }
+
             float t = Mathf.InverseLerp(first.Time, second.Time, time);
-            return first.Interpolate(second, t);
+            Value = first.Interpolate(second, t);
+            return Value;
         }
 
         // Binary search for the keyframe pair that contains the given time
@@ -80,9 +123,6 @@ namespace RTFunctions.Functions.Animation
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        T ResultFromSingleKeyframe(IKeyframe<T> keyframe)
-        {
-            return keyframe.Interpolate(keyframe, 0.0f);
-        }
+        T ResultFromSingleKeyframe(IKeyframe<T> keyframe, float t) => keyframe.Interpolate(keyframe, t);
     }
 }
