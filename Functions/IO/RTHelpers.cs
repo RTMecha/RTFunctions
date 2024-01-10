c1dbec769f082ab5c4015b29b46436b35b9dd813
RTMecha <mechaprower2022@gmail.com>
2024-01-11 00:06:13 +1100

    Working on special events

--- Functions/IO/RTHelpers.cs
+++ Functions/IO/RTHelpers.cs
@@ -53,6 +53,8 @@ namespace RTFunctions.Functions.IO
 		public static bool Paused => GameManager.inst && GameManager.inst.gameState == GameManager.State.Paused;
 		public static bool Playing => GameManager.inst && GameManager.inst.gameState == GameManager.State.Playing;
 
+		public static bool AprilFools => System.DateTime.Now.ToString("M") == "1 April";
+
 		public static float getPitch()
         {
 			if (EditorManager.inst != null)
