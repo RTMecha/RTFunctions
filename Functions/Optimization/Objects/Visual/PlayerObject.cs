57bb1e70280495b9045f27646d970fe328c7531f
RTMecha <mechaprower2022@gmail.com>
2024-01-16 00:36:09 +1100

    Updated PlayerObject

--- Functions/Optimization/Objects/Visual/PlayerObject.cs
+++ Functions/Optimization/Objects/Visual/PlayerObject.cs
@@ -14,32 +14,15 @@ namespace RTFunctions.Functions.Optimization.Objects.Visual
         public override Renderer Renderer { get; set; }
         public override Collider2D Collider { get; set; }
 
-        readonly Material material;
-        readonly float opacity;
-
-        public PlayerObject(GameObject gameObject, Transform top, float opacity, bool hasCollider, bool solid = false)
+        public PlayerObject(GameObject gameObject, Transform top)
         {
             GameObject = gameObject;
             Top = top;
-
-            this.opacity = opacity;
-
-            Renderer = gameObject.GetComponent<Renderer>();
-            Renderer.enabled = true;
-            material = Renderer.material;
-
-            Collider = gameObject.GetComponent<Collider2D>();
-
-            if (Collider != null)
-            {
-                Collider.enabled = true;
-                if (hasCollider)
-                    Collider.tag = "Helper";
-                if (solid)
-                    Collider.isTrigger = false;
-            }
         }
 
-        public override void SetColor(Color color) => material?.SetColor(new Color(color.r, color.g, color.b, color.a * opacity));
+        public override void SetColor(Color color)
+        {
+
+        }
     }
 }
