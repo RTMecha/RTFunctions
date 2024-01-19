db9ff53cdf45333ded39ad955c2a28a170167547
RTMecha <mechaprower2022@gmail.com>
2024-01-19 14:56:35 +1100

    Version: 1.9.1

--- FunctionsPlugin.cs
+++ FunctionsPlugin.cs
@@ -34,7 +34,7 @@ namespace RTFunctions
 	/// <summary>
 	/// Base plugin for initializing all the patches.
 	/// </summary>
-	[BepInPlugin("com.mecha.rtfunctions", "RT Functions", " 1.9.0")]
+	[BepInPlugin("com.mecha.rtfunctions", "RT Functions", " 1.9.1")]
 	[BepInProcess("Project Arrhythmia.exe")]
 	public class FunctionsPlugin : BaseUnityPlugin
 	{
