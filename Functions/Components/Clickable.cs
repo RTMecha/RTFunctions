c56aa596eac5ff2d67e5a1d7d8e35f67fc00fb16
RTMecha <mechaprower2022@gmail.com>
2024-02-24 18:56:53 +1100

    Added onEnable delegate

--- Functions/Components/Clickable.cs
+++ Functions/Components/Clickable.cs
@@ -7,6 +7,8 @@ namespace RTFunctions.Functions.Components
 {
     public class Clickable : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
+        public Action<bool> onEnable;
+
         public Action<PointerEventData> onClick;
         public Action<PointerEventData> onDown;
         public Action<PointerEventData> onEnter;
@@ -16,6 +18,9 @@ namespace RTFunctions.Functions.Components
         public Action<PointerEventData> onBeginDrag;
         public Action<PointerEventData> onDrag;
         public Action<PointerEventData> onEndDrag;
+
+        void OnEnable() => onEnable?.Invoke(true);
+        void OnDisable() => onEnable?.Invoke(false);
 
         public void OnBeginDrag(PointerEventData pointerEventData) => onBeginDrag?.Invoke(pointerEventData);
         public void OnDrag(PointerEventData pointerEventData) => onDrag?.Invoke(pointerEventData);
