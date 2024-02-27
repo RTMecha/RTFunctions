b7b0585b33606fc50470fb0dbd0b3ad96b3d692c
RTMecha <mechaprower2022@gmail.com>
2024-02-28 00:29:28 +1100

    Alive Count debug

--- Functions/Managers/RTDebugger.cs
+++ Functions/Managers/RTDebugger.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Linq;
+
+using RTFunctions.Functions.Data;
 
 namespace RTFunctions.Functions.Managers
 {
     public static class RTDebugger
     {
         public static void TogglePlay() => (AudioManager.inst.CurrentAudioSource.isPlaying ? (Action)AudioManager.inst.CurrentAudioSource.Pause : AudioManager.inst.CurrentAudioSource.Play).Invoke();
+
+        public static int BeatmapObjectAliveCount() => GameData.Current.BeatmapObjects.Where(x => x.objectType != BeatmapObject.ObjectType.Empty && x.TimeWithinLifespan()).Count();
     }
 }
