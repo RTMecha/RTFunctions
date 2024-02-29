ea7cafafaa86bc3b113fc86992642e85609f22ca
RTMecha <mechaprower2022@gmail.com>
2024-02-29 21:40:37 +1100

    Debug colors for UnityExplorer

--- Functions/Managers/RTDebugger.cs
+++ Functions/Managers/RTDebugger.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Linq;
 
+using UnityEngine;
+using LSFunctions;
 using RTFunctions.Functions.Data;
+using RTFunctions.Functions.IO;
 
 namespace RTFunctions.Functions.Managers
 {
@@ -10,5 +13,8 @@ namespace RTFunctions.Functions.Managers
         public static void TogglePlay() => (AudioManager.inst.CurrentAudioSource.isPlaying ? (Action)AudioManager.inst.CurrentAudioSource.Pause : AudioManager.inst.CurrentAudioSource.Play).Invoke();
 
         public static int BeatmapObjectAliveCount() => GameData.Current.BeatmapObjects.Where(x => x.objectType != BeatmapObject.ObjectType.Empty && x.TimeWithinLifespan()).Count();
+
+        public static void LogColor(Color color) => Debug.Log($"[<color=#{RTHelpers.ColorToHex(color)}>▓▓▓▓▓▓▓▓▓▓▓▓▓</color>]");
+        public static void LogColor(string color) => Debug.Log($"[<color={color}>▓▓▓▓▓▓▓▓▓▓▓▓▓</color>]");
     }
 }
